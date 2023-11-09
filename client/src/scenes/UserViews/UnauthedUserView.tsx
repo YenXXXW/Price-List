cf4af5cf1b3274efe9dc49f7a5361ceefe931ae3
Yen <YenXXXW@gmail.com>
2023-11-09 22:51:55 +0630

    debugging

--- client/src/scenes/UserViews/UnauthedUserView.tsx
+++ client/src/scenes/UserViews/UnauthedUserView.tsx
@@ -6,12 +6,12 @@ const UnauthedUserView = () => {
     <section>
       <p className="font-bold ">Please Login In or SignUp to use the service</p>
       <div className="flex flex-col gap-1 w-[100px]">
-        <button className="authButton" onClick={() => navigate("/login")}>
+        {/* <button className="authButton" onClick={() => navigate("/login")}>
           Log In
         </button>
         <button className="authButton" onClick={() => navigate("/signup")}>
           Sign Up
-        </button>
+        </button> */}
       </div>
     </section>
   );
