1847fff2b2f323246edbed50a856178836baa08c
Yen <YenXXXW@gmail.com>
2023-11-09 22:39:21 +0630

    UnAUthedUserView button naviation added

--- client/src/scenes/UserViews/UnauthedUserView.tsx
+++ client/src/scenes/UserViews/UnauthedUserView.tsx
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 const UnauthedUserView = () => {
+  const navigate = useNavigate();
   return (
     <section>
       <p className="font-bold ">Please Login In or SignUp to use the service</p>
       <div className="flex flex-col gap-1 w-[100px]">
-        <button className="authButton">Log In</button>
-        <button className="authButton">Sign Up</button>
+        <button className="authButton" onClick={() => navigate("/login")}>
+          Log In
+        </button>
+        <button className="authButton" onClick={() => navigate("/signup")}>
+          Sign Up
+        </button>
       </div>
     </section>
   );
