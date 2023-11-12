0d252b938f7fd6f15d6dde6ce8012ed18a54e920
Yen <YenXXXW@gmail.com>
2023-11-12 21:31:13 +0630

    deploy debugging

--- server/src/app.ts
+++ server/src/app.ts
@@ -25,7 +25,10 @@ app.set("trust proxy", 1);
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173/",
+      "https://price-list-yenxxxw.vercel.app/",
+    ],
     optionsSuccessStatus: 200,
     credentials: true,
   })
