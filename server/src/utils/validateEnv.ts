bc855d09d13aa1acb28eb750cebf5f5022a1d6ad
Yen <YenXXXW@gmail.com>
2023-11-12 22:17:24 +0630

    envalid error

--- server/src/utils/validateEnv.ts
+++ server/src/utils/validateEnv.ts
@@ -4,7 +4,7 @@ export default cleanEnv(process.env, {
   MONGO_URL: str(),
   PORT: port(),
   SESSION_SECRET: str(),
-  NODE_ENV: str(),
-  ALLOWED_DEPLOYED_URL: str(),
-  LOCAL_HOST_ADDRESS: str(),
+  // NODE_ENV: str(),
+  // ALLOWED_DEPLOYED_URL: str(),
+  // LOCAL_HOST_ADDRESS: str(),
 });
